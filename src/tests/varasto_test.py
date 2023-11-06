2f63080ec16e8bfb4a43317d5d8290da3738f8fa
mjouppil <markkujouppila@outlook.com>
2023-11-06 13:19:21 +0200

    Päivitetty varasto_test.py testit olemaan 100%.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,7 +5,10 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
-
+        self.varastoNeg = Varasto(-1)
+        self.varastoSaldoNeg = Varasto(10, -1)
+        self.varastoOver = Varasto(10, 11)
+        
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
@@ -38,3 +41,48 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_lisaa_negatiivinen_maara(self):
+        sal1 = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, sal1)
+        
+    def test_ota_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(10)
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara, 0)
+        
+    def test_varastoNeg_oikea_tilavuus(self):
+        self.assertAlmostEqual(self.varastoNeg.tilavuus, 0)
+        
+    def test_varastoSaldoNeg_oikea_saldo(self):
+        self.assertAlmostEqual(self.varastoSaldoNeg.saldo, 0)
+        
+    def test_varastoOver_oikea_saldo(self):
+        self.assertAlmostEqual(self.varastoOver.saldo, 10)
+        
+    def test_ota_kaikki(self):
+        sal1 = self.varasto.saldo
+        saatu_maara = self.varasto.ota_varastosta(sal1)
+        self.assertAlmostEqual(saatu_maara, sal1)
+        
+    def test_lisaa_tayteen(self):
+        self.varasto.lisaa_varastoon(5)
+        lis1 = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(lis1)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+        
+    def test_ota_liikaa(self):
+        self.varasto.ota_varastosta(100)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+        
+    def test_tulosta_tiedot(self):
+    	print(self.varasto)
+        
+        
+        
+        
